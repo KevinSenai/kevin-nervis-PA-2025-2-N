4aed5681da3f3df22c528e4c9f619b47e203f037
KevinSenai <kevin_n_pereira@estudante.sesisenai.org.br>
2026-02-26 21:33:43 -0300

    Metodos Update

--- Metodos/src/Atv1.java
+++ Metodos/src/Atv1.java
@@ -1,33 +1,41 @@
 import java.util.Scanner;
-
-public class Main {
+public class Atv1 {
     public static void main(String[] args) {
-        estadoEpoca();
-    }
-
-    public static void estadoEpoca() {
         System.out.println("Informe a Enstão \n1 - Primavera\n2 - Verão \n3 - Outono \n4 - Inverno");
         Scanner sc = new Scanner(System.in);
+
         int numero = sc.nextInt();
         switch (numero) {
             case 1:
-                System.out.println("As Flores estão Abrochando");
+                primavera();
                 break;
             case 2:
-                System.out.println("É Verão bora praia");
+                verao();
                 break;
             case 3:
-                System.out.println("Flor Cainda Bem vindo ao Outono");
+                outono();
                 break;
             case 4:
-                System.out.println("Esta Frio la fora, Bem vindo ao Inverno");
+                inverno();
                 break;
             default:
                 System.out.println("Numero Invalido!!");
+
         }
     }
-    public static void amigos(){
+    public static void primavera() {
+        System.out.println("Flores estão desabrochando");
+    }
+    public static void verao() {
+        System.out.println("É Verão bora praia");
 
     }
+    public static void  outono(){
+        System.out.println("Flor Cainda Bem vindo ao Outono");
+    }
+    public static void inverno(){
+        System.out.println("Esta Frio la fora, Bem vindo ao Inverno");
+    }
 }
+
 
