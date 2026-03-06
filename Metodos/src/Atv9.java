23c25b6b7230bef753ea252d2138da39d8ed0ef6
KevinSenai <kevin_n_pereira@estudante.sesisenai.org.br>
2026-03-05 21:52:09 -0300

    Update Atv8,9,7

---
+++ Metodos/src/Atv9.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Atv9 {
+    public static boolean parImpar(int n){
+        if(n % 2 == 0 ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe Numero: ");
+        int n = sc.nextInt();
+        System.out.println(parImpar(n));
+    }
+}
